07c717423cb5dd53d5afb537b6a3a6639b32f8ac
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-03-09 12:35:43 +0530

    Add Block_Swap_algorithm class to implement block swapping functionality

---
+++ PLA/Block_Swap_algorithm.java
@@ -0,0 +1,32 @@
+package PLA;
+
+public class Block_Swap_algorithm {
+    public static void Swap_algorithm(int[] arr, int k, int n){
+        if(n == 0){
+            return ;
+        }
+        if((k % n) > n){
+            return;
+        }
+        int[] temp = new int[k];
+        for(int i=0; i<k; i++){
+            temp[i] = arr[i];
+        }
+        for(int i=0; i<n-k; i++){
+            arr[i] = arr[i+k];
+        }
+        for(int i=n-k; i<n; i++){
+            arr[i] = temp[i-n+k];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int k = 2;
+        int n = arr.length;
+
+        Swap_algorithm(arr, k, n);
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
