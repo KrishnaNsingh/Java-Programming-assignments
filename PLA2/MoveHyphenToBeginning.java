8cc31d5f86c2e76923a67cf52533705dbb8cdc98
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-04-19 23:42:21 +0530

    Add moveHyphenToBeginning class to move hyphens to the beginning of a string

---
+++ PLA2/MoveHyphenToBeginning.java
@@ -0,0 +1,27 @@
+package PLA2;
+
+public class moveHyphenToBeginning {
+    public static String moveHyphen(String s) {
+        int hyphencount = 0;
+        String word = "";
+        
+        for(char i : s.toCharArray()){
+            if(i == '-'){
+                hyphencount++;
+            }else{
+                word += i;
+            }
+        }
+
+        String hyphen = "";
+        for(int i=0; i<hyphencount; i++){
+            hyphen += '-';
+        }
+
+        return hyphen + word; 
+    }
+    public static void main(String[] args) {
+        String s = "ab-cd-eg-";
+        System.out.println(moveHyphen(s));
+    }
+}
