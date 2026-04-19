6cd6c63da4add06f5d78722f9d2108d0dd152f12
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-04-19 23:30:58 +0530

    Add Weighted_Substring class to calculate weighted sum of characters in a string

---
+++ PLA2/Weighted_Substring.java
@@ -0,0 +1,17 @@
+package PLA2;
+
+public class Weighted_Substring {
+    public static int WeightedString(String s){
+        int sum = 0;
+        for(int i=0; i<s.length(); i++){
+            int value = s.charAt(i) - 'a' + 1;
+            sum += value * (i+1);
+        }
+
+        return sum;
+    }
+    public static void main(String[] args){
+        String s = "abc";
+        System.out.println(WeightedString(s));
+    }
+}
