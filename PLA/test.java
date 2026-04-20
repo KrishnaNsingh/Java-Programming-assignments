bb4636b7d331192ca9eca3d1eadbc3c72fb49eff
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-04-20 19:46:45 +0530

    Refactor test class: remove equilibriumPosition method and implement GCD method

--- PLA/test.java
+++ PLA/test.java
@@ -1,34 +1,20 @@
 package PLA;
 
+import java.util.*;
 
-
-// import java.util.*;
 public class test {
-    static int equlibriumPosition(int[] arr){
-        int n = arr.length;
-        int[] preSum = new int[n];
-        int[] suffsum = new int[n];
-
-        preSum[0] = arr[0];
-        for(int i=1; i<n; i++){
-            preSum[i] = preSum[i-1] + arr[i];
+    public static int GCD(int a, int b){
+        while(b != 0){
+            int ans = a % b;
+            a = b;
+            b = ans;
         }
-
-        suffsum[n-1] = arr[n-1];
-        for(int i=n-2; i>=0; i--){
-            suffsum[i] = suffsum[i+1] + arr[i];
-        }
-
-        for(int i=1; i<n; i++){
-            if(preSum[i-1] == suffsum[i+1]){
-                return i;
-            }
-        }
-        return -1;
+        return a;
     }
     public static void main(String[] args) {
-        int[] arr = { -2, 5, 3, 3, 2, 6, -4, 2 };
-        int result = equlibriumPosition(arr);
-        System.out.println("Equlibrium Position is : "+ result);
+        int a = 48;
+        int b = 6;
+        int answer = GCD(a, b);
+        System.out.println(answer);
     }
 }
